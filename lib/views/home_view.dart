7d0bfbb2b331251615a37f983bd09247b545abc1
“3kdeveloper” <kamranktk807@gmail.com>
2026-04-07 19:12:15 +0500

    feat: Integrate device preview and update dependencies
    - Added device_preview package for enhanced device simulation during development.
    - Replaced responsive_scaler with sizer for responsive design.
    - Updated main.dart to utilize Sizer for scaling.
    - Refactored HomeView to demonstrate Sizer usage.
    - Removed unused responsive approach views.
    - Updated Podfile for iOS and macOS to include shared_preferences_foundation.
    - Added necessary configurations for CocoaPods in both iOS and macOS.
    - Updated pubspec.yaml and pubspec.lock to reflect new dependencies and versions.

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // double physicalHeight = MediaQuery.sizeOf(context).height;
+    // print('Height: $physicalHeight');
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Responsive Approaches'),
-        backgroundColor: Colors.blueGrey,
-      ),
-      body: SizedBox.expand(
+      // appBar: AppBar(title: Text('Sizer'), backgroundColor: Colors.blueGrey),
+      body: SafeArea(
+        bottom: false,
         child: Column(
-          mainAxisAlignment: .center,
-          crossAxisAlignment: .center,
           spacing: 16,
           children: [
-            ElevatedButton(onPressed: () {}, child: Text('Sizer')),
-            ElevatedButton(onPressed: () {}, child: Text('Responsive Scaler')),
-            ElevatedButton(onPressed: () {}, child: Text('Flutter ScreenUtil')),
-            ElevatedButton(onPressed: () {}, child: Text('Manual Scaling')),
+            // Container(height: 100, width: .maxFinite, color: Colors.blueGrey),
+            Container(
+              height: 100.px,
+              width: .maxFinite,
+              color: Colors.blueGrey,
+              alignment: .center,
+              child: Text(
+                'Sizer',
+                style: TextStyle(fontSize: 24.sp, color: Colors.white),
+                // 15.sp can also be used instead of .dp
+                // To know their differences, check #FAQ
+              ),
+            ),
+            Container(
+              height: 100,
+              width: .maxFinite,
+              color: Colors.blueGrey,
+              alignment: .center,
+              child: Text(
+                'Sizer',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+                // 15.sp can also be used instead of .dp
+                // To know their differences, check #FAQ
+              ),
+            ),
+
+            // Container(height: 40.px, width: 40.px, color: Colors.red),
           ],
         ),
       ),
