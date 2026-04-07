73a91e9899f93057a32c2d63067819c8db816987
“3kdeveloper” <kamranktk807@gmail.com>
2026-04-07 14:21:54 +0500

    feat: Implement ResponsiveScalerView and update main app structure

---
+++ lib/views/responsive_approaches/responsive_scaler_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveScalerView extends StatelessWidget {
+  const ResponsiveScalerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Responsive Scaler')));
+  }
+}
